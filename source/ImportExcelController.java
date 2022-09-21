6e0efb0520227a9020af5c445200d881606f067f
kbanpmt <mju6104106301@mju.ac.th>
2022-09-21 18:44:55 +0700

    3 files Import Excel File

---
+++ source/ImportExcelController.java
@@ -0,0 +1,192 @@
+package controller;
+
+import java.io.*;
+import java.util.*;
+import lombok.*;
+import manager.ImportExcelManager;
+import manager.ListStudentProjectManager;
+import bean.*;
+import util.*;
+//import manager.*;
+
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@MultipartConfig
+public class ImportExcelController {
+
+//	@RequestMapping(value = "/doAddStudentProject", method = RequestMethod.GET)
+//	public ModelAndView doAddStudentProjectPage(HttpSession session, HttpServletRequest request) throws Exception {
+//		Admin admin = (Admin) session.getAttribute("admin");
+//		if (admin != null) {
+//			ImportExcelManager importExcelManager = new ImportExcelManager();
+//			List<ProjectType> projectTypeList = importExcelManager.getListProjectType();
+//			ModelAndView mav = new ModelAndView("AddStudentProject");
+//			mav.addObject("projectTypeList", projectTypeList);
+//			return mav;
+//		} else {
+//			ModelAndView mav = new ModelAndView("LoginPage");
+//			mav.addObject("msg", "กรุณาเข้าสู่ระบบใหม่อีกครั้ง!!!!");
+//			session.removeAttribute("admin");
+//			return mav;
+//		}
+//	}
+
+	@RequestMapping(value = "/ImportExcel", method = RequestMethod.POST)
+	public ModelAndView doImportExcel(HttpSession session, HttpServletRequest request, HttpServletResponse response)throws Exception {
+		Admin admin = (Admin) session.getAttribute("admin");
+		if (admin != null) {
+			if (ServletFileUpload.isMultipartContent(request)) {
+				request.setCharacterEncoding("UTF-8");
+				
+				try {
+					ImportExcelManager importExcelManager = new ImportExcelManager();
+					
+					List<FileItem> multiFileItem = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+
+					val fileName = multiFileItem.get(0).getName();
+
+					System.out.println("fileName : " + fileName);
+					
+					String path = request.getSession().getServletContext().getRealPath("/") + "WEB-INF/resources/excels";
+					
+					val file = path + File.separator + System.currentTimeMillis() +"_"+ fileName;
+					
+					multiFileItem.get(0).write(new File(file));
+
+					ReadWriteExcel readWriteExcel = new ReadWriteExcel();
+					
+					System.out.println(file);
+					
+					try {
+						readWriteExcel.xlRead(file, 0);
+						// System.out.println(readWriteExcel.getxRows()); 
+						String[][] data = readWriteExcel.getData();
+						// System.out.println(data[0][0]); 
+						for (int i = 1; i < data.length; i++) {
+
+							val student_id = Integer.parseInt(data[i][0]);
+							val prefix = data[i][1];
+							val firstname = data[i][2];
+							val lastname = data[i][3];
+							val grade = data[i][4];
+							val mobileno = data[i][5];
+							val email = data[i][6];
+							val password = data[i][7];
+							val school_id = Integer.parseInt(data[i][8]);
+							val school_name = data[i][9];
+							val school_address= data[i][10];
+							val advisor_id= Integer.parseInt(data[i][11]);
+							val advprefix= data[i][12];
+							val advfirstname= data[i][13];
+							val advlastname= data[i][14];
+							val advemail= data[i][15];
+							val advmobileno= data[i][16];
+							val project_id= data[i][17];
+							val classproject= data[i][18];
+							val projectname= data[i][19];
+							val video = data[i][20];
+							val award = data[i][21];
+							val avgscore = Double.parseDouble(data[i][22]);
+							val projecttype_id = Integer.parseInt(data[i][23]);
+							val projecttype_name = data[i][24];
+							
+							Student student = new Student();
+							School school = new School();
+							Advisor advisor = new Advisor();
+							Project project = new Project();
+							ProjectType projecttype = new ProjectType();
+							
+							school.setSchool_id(school_id);
+							school.setSchool_name(school_name);
+							school.setSchool_address(school_address);
+							
+							student.setStudent_id(student_id);
+							student.setPrefix(prefix);
+							student.setFirstname(firstname);
+							student.setLastname(lastname);
+							student.setGrade(grade);
+							student.setMobileno(mobileno);
+							student.setEmail(email);
+							student.setPassword(password);
+							student.setSchool(school);			
+
+							advisor.setAdvisor_id(advisor_id);
+							advisor.setPrefix(advprefix);
+							advisor.setFirstname(advfirstname);
+							advisor.setLastname(advlastname);
+							advisor.setEmail(advemail);
+							advisor.setMobileno(advmobileno);
+							
+							projecttype.setProjecttype_id(projecttype_id);
+							projecttype.setProjecttype_name(projecttype_name);
+													
+							project.setProject_id(project_id);
+							project.setClassproject(classproject);
+							project.setProjectname(projectname);
+							project.setVideo(video);
+							project.setAward(award);
+							project.setAvgscore(avgscore);
+							project.setProjecttype(projecttype);
+														
+							School schoolTemp = importExcelManager.getSchoolByID(school_id);
+							ProjectType projectTypeTemp = importExcelManager.getProjectTypeByID(projecttype_id);
+							Project projectTemp = importExcelManager.getProjectByID(project_id);
+							Advisor advisorTemp = importExcelManager.getAdvisorByID(advisor_id);
+							
+							if (schoolTemp.getSchool_id() != school_id) {
+								importExcelManager.isImportSchool(school);
+							};
+							
+							importExcelManager.isImportStudent(student);
+							
+							if (projectTypeTemp.getProjecttype_id() != projecttype_id) {
+								importExcelManager.isImportProjectType(projecttype);
+							};
+							
+							if (projectTemp.getProject_id() != project_id) {
+								importExcelManager.isImportProject(project);
+							};
+							
+							if (advisorTemp.getAdvisor_id() != advisor_id) {
+								importExcelManager.isImportAdvisor(advisor);
+							};
+					
+							importExcelManager.isImportStudentData(student, project, advisor);
+							
+						}
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			ListStudentProjectManager vpjm = new ListStudentProjectManager();
+			List<StudentProject> listsproject = vpjm.getListStudentProject();
+			ModelAndView mav = new ModelAndView("ListStudentProject");
+			mav.addObject("listsproject", listsproject);
+			return mav;
+		} else {
+			ModelAndView mav = new ModelAndView("LoginPage");
+			mav.addObject("msg", "กรุณาเข้าสู่ระบบใหม่อีกครั้ง!!!!");
+			session.removeAttribute("admin");
+			return mav;
+		}
+
+	}
+
+}
---
+++ source/ImportExcelManager.java
@@ -0,0 +1,318 @@
+package manager;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Vector;
+
+import bean.*;
+import resultset.ResultSetToClass;
+import util.ConnectionDB;
+
+public class ImportExcelManager {
+	
+	ResultSetToClass resultSetToClass = new ResultSetToClass();
+	
+	public List<ProjectType> getListProjectType() throws Exception {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Statement stmt = null;
+		List<ProjectType> projectTypeList = new Vector<>();
+		
+		try {
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM projecttype " ;
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {	
+					
+				projectTypeList.add(resultSetToClass.setResultSetToProjectType(rs));
+
+			}
+
+			con.close();
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("catch");
+			e.printStackTrace();
+		}
+		return projectTypeList;
+	}
+	
+	public boolean isImportSchool(School school) {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Boolean result = false;
+
+		try {
+			CallableStatement stmt = con.prepareCall("{call isImportSchool(?,?,?)}");
+			
+			stmt.setInt(1, school.getSchool_id());
+			stmt.setString(2, school.getSchool_name());
+			stmt.setString(3, school.getSchool_address());
+					
+			stmt.execute();
+
+			result = true;
+			con.close();
+			stmt.close();
+		} catch (SQLException er) {
+			er.printStackTrace();
+		}
+		return result;
+	}
+	
+	public boolean isImportStudent(Student student) {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Boolean result = false;
+
+		try {
+			CallableStatement stmt = con.prepareCall("{call isImportStudent(?,?,?,?,?,?,?,?,?)}");
+			stmt.setInt(1, student.getStudent_id());
+			stmt.setString(2, student.getPrefix());
+			stmt.setString(3, student.getFirstname());
+			stmt.setString(4, student.getLastname());
+			stmt.setString(5, student.getGrade());
+			stmt.setString(6, student.getMobileno());
+			stmt.setString(7, student.getEmail());
+			stmt.setString(8, student.getPassword()); 
+			stmt.setInt(9, student.getSchool().getSchool_id());
+							
+			stmt.execute();
+
+			result = true;
+			con.close();
+			stmt.close();
+		} catch (SQLException er) {
+			er.printStackTrace();
+		}
+		return result;
+	}
+	
+	public boolean isImportProjectType(ProjectType projectType) {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Boolean result = false;
+
+		try {
+			CallableStatement stmt = con.prepareCall("{call isImportProjectType(?,?)}");
+			
+			stmt.setInt(1, projectType.getProjecttype_id());
+			stmt.setString(2, projectType.getProjecttype_name());
+					
+			stmt.execute();
+
+			result = true;
+			con.close();
+			stmt.close();
+		} catch (SQLException er) {
+			er.printStackTrace();
+		}
+		return result;
+	}
+	
+	public boolean isImportProject(Project project) {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Boolean result = false;
+
+		try {
+			CallableStatement stmt = con.prepareCall("{call isImportProject(?,?,?,?,?,?,?)}");
+		
+			stmt.setString(1, project.getProject_id());
+			stmt.setString(2, project.getClassproject());
+			stmt.setString(3, project.getProjectname());
+			stmt.setString(4, project.getVideo());
+			stmt.setString(5, project.getAward());
+			stmt.setDouble(6, project.getAvgscore());	
+			stmt.setInt(7, project.getProjecttype().getProjecttype_id());
+
+					
+			stmt.execute();
+
+			result = true;
+			con.close();
+			stmt.close();
+		} catch (SQLException er) {
+			er.printStackTrace();
+		}
+		return result;
+	}
+	
+	public boolean isImportAdvisor(Advisor advisor) {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Boolean result = false;
+
+		try {
+			CallableStatement stmt = con.prepareCall("{call isImportAdvisor(?,?,?,?,?,?)}");
+
+			stmt.setInt(1, advisor.getAdvisor_id());
+			stmt.setString(2, advisor.getPrefix());
+			stmt.setString(3, advisor.getFirstname());
+			stmt.setString(4, advisor.getLastname());
+			stmt.setString(5, advisor.getEmail());
+			stmt.setString(6, advisor.getMobileno());
+					
+			stmt.execute();
+
+			result = true;
+			con.close();
+			stmt.close();
+		} catch (SQLException er) {
+			er.printStackTrace();
+		}
+		return result;
+	}
+	
+	public boolean isImportStudentData(Student student, Project project, Advisor advisor) {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Boolean result = false;
+
+		try {
+			CallableStatement stmt = con.prepareCall("{call isImportStudentData(?,?,?)}");
+
+			stmt.setInt(1, student.getStudent_id());
+			stmt.setString(2, project.getProject_id());
+			stmt.setInt(3, advisor.getAdvisor_id());
+
+					
+			stmt.execute();
+
+			result = true;
+			con.close();
+			stmt.close();
+		} catch (SQLException er) {
+			er.printStackTrace();
+		}
+		return result;
+	}
+	
+	public School getSchoolByID(Integer school_id) throws Exception {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Statement stmt = null;
+		School school = new School();
+		
+		try {
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM school WHERE school_id = '"+ school_id +"' " ;
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {	
+					
+				school.setSchool_id(rs.getInt("school.school_id"));
+				school.setSchool_name(rs.getString("school.school_name"));
+				school.setSchool_address(rs.getString("school.school_address"));
+				
+			}
+
+			con.close();
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("catch");
+			e.printStackTrace();
+		}
+		return school;
+	}
+	
+	public ProjectType getProjectTypeByID(Integer projecttype_id) throws Exception {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Statement stmt = null;
+		ProjectType projectType = new ProjectType();
+		
+		try {
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM projecttype WHERE projecttype_id = '"+ projecttype_id +"' " ;
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {	
+					
+				projectType.setProjecttype_id(rs.getInt("projecttype.projecttype_id"));
+				projectType.setProjecttype_name(rs.getString("projecttype.projecttype_name"));
+				
+			}
+
+			con.close();
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("catch");
+			e.printStackTrace();
+		}
+		return projectType;
+	}
+	
+	public Project getProjectByID(String project_id) throws Exception {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Statement stmt = null;
+		Project project = new Project();
+		
+		try {
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM project WHERE project_id = '"+ project_id +"' " ;
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {	
+					
+				project.setProject_id(rs.getString("project.project_id"));
+				project.setClassproject(rs.getString("project.classproject"));
+				project.setProjectname(rs.getString("project.projectname"));		
+				project.setVideo(rs.getString("project.video"));
+				project.setAward(rs.getString("project.award"));
+				project.setAvgscore(rs.getDouble("project.avgscore"));
+				
+			}
+
+			con.close();
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("catch");
+			e.printStackTrace();
+		}
+		return project;
+	}
+	
+	public Advisor getAdvisorByID(Integer advisor_id) throws Exception {
+		ConnectionDB condb = new ConnectionDB();
+		Connection con = condb.getConnection();
+		Statement stmt = null;
+		Advisor advisor = new Advisor();
+		
+		try {
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM advisor WHERE advisor_id = '"+ advisor_id +"' " ;
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {	
+					
+				advisor.setAdvisor_id(rs.getInt("advisor.advisor_id"));
+				advisor.setPrefix(rs.getString("advisor.prefix"));	
+				advisor.setFirstname(rs.getString("advisor.firstname"));	
+				advisor.setLastname(rs.getString("advisor.lastname"));	
+				advisor.setEmail(rs.getString("advisor.email"));
+				advisor.setMobileno(rs.getString("advisor.mobileno"));	
+				
+			}
+
+			con.close();
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("catch");
+			e.printStackTrace();
+		}
+		return advisor;
+	}
+
+}
